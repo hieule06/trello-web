8c8579cb3f9066531d9aeb2e024ffe1fbdee1fcf
hieule06 <127398331+hieule06@users.noreply.github.com>
2024-03-19 13:40:49 +0700

    add duck mode and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import { Button, Typography } from "@mui/material";
 import "./App.css";
 import { HomeRepairServiceOutlined } from "@mui/icons-material";
 import { pink } from "@mui/material/colors";
+import { useColorScheme } from "@mui/material/styles";
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>hello world</div>
       <Typography variant="body-2" color={"text.secondary"}>
         123
